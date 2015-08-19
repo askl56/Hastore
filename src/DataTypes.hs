bc6d198c4a181b9cc68c024f31cd51d4c59f6752
askl56 <amscott@openmailbox.org>
2015-08-19 13:01:57 +0100

    further into recursion

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -45,3 +45,26 @@ fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+f :: Client -> String
+f client = case client of
+             Company _ _ (Person name _ _) "Boss" -> name ++ " is the boss"
+             _                                  -> "There is no boss"
+
+g :: Client -> String
+g client = case client of
+             Company _ _ (Person name _ _) pos ->
+               case pos of "Boss" -> name ++ " is the boss"
+             _                               -> "There is no boss"
+
+(+++) :: [a] -> [a] -> [a]
+[]     +++ list2 = list2
+(x:xs) +++ list2 = x:(xs +++ list2)
+{-list1 +++ list2 = case list1 of
+                    []   -> list2
+                    x:xs -> x:(xs +++ list2)-}
+
+sorted :: [Integer] -> Bool
+sorted []  = True
+sorted [_] = True
+sorted (x : r@(y:_)) = x < y && sorted r
